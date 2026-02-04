49882ecb3d8779ff89ced47fdba09d9a60377ac0
Abdurauf Abdullayev <abdullayevaxmadullo977@gmail.com>
2026-02-04 18:03:34 +0500

    changed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,62 +4,75 @@ import QRCode from '../assets/images/Vector.svg';
 
 const Home = () => {
   return (
-    <section className="chek">
-      <img src={logoChek} className="logo" />
-      <img src={naqsh} className="naqsh" />
-      <div className="chek-content">
-        <h2>
-          <span>F.I.O.:</span>
-          Abdumannobov Muhammadibrohim Mahammadjon o‘g‘li
-        </h2>
-        <h2>
-          <span>Filial:</span>
-          Asosiy filial
-        </h2>
-        <h2>
-          <span>Kurs:</span>
-          Arab tili
-        </h2>
-        <h2>
-          <span>Guruh:</span>
-          AF-01-20
-        </h2>
-        <h2>
-          <span>O‘qtuvchi::</span>
-          X.S.Qodirov
-        </h2>
-        <h2>
-          <span>Summa:</span>
-          350 000 so‘m
-        </h2>
-        <h2>
-          <span>To‘lov turi:</span>
-          Naqd
-        </h2>
-        <h2>
-          <span>To‘lov ID:</span>
-          51872
-        </h2>
-        <h2>
-          <span>To‘lov sanasi:</span>
-          2024-10-17
-        </h2>
-        <h2>
-          <span>To‘lov vaqti:</span>
-          11:45:00
-        </h2>
-        <h2>
-          <span>Kassir:</span>
-          I.I. Obidov
-        </h2>
-      </div>
-      <div className="chek-bottom">
-        <h1>“ILMPLYUS O‘QUV” NTM</h1>
-        <h1>STIR | 303451853</h1>
-        <img src={QRCode} className="qr-code" />
-        <p>www.ilmplyus.uz</p>
-      </div>
-    </section>
+    <>
+      <button onClick={() => window.print()}>PDF chiqarish</button>
+      <section className="chek">
+        <img src={logoChek} className="logo" />
+        <img src={naqsh} className="naqsh" />
+        <div className="chek-content">
+          <div className="chek-row">
+            <span>F.I.O:</span>
+            <span>Abdumannobov Muhammadibrohim Mahammadjon o‘g‘li</span>
+          </div>
+
+          <div className="chek-row">
+            <span>Filial:</span>
+            <span>Asosiy filial</span>
+          </div>
+
+          <div className="chek-row">
+            <span>Kurs:</span>
+            <span>Arab tili</span>
+          </div>
+
+          <div className="chek-row">
+            <span>Guruh:</span>
+            <span>AF-01-20</span>
+          </div>
+
+          <div className="chek-row">
+            <span>O‘qituvchi:</span>
+            <span>X.S.Qodirov</span>
+          </div>
+
+          <div className="chek-row">
+            <span>Summa:</span>
+            <span>350 000 so‘m</span>
+          </div>
+
+          <div className="chek-row">
+            <span>To‘lov turi:</span>
+            <span>Naqd</span>
+          </div>
+
+          <div className="chek-row">
+            <span>To‘lov ID:</span>
+            <span>51872</span>
+          </div>
+
+          <div className="chek-row">
+            <span>To‘lov sanasi:</span>
+            <span>2024-10-17</span>
+          </div>
+
+          <div className="chek-row">
+            <span>To‘lov vaqti:</span>
+            <span>11:45:00</span>
+          </div>
+
+          <div className="chek-row">
+            <span>Kassir:</span>
+            <span>I.I. Obidov</span>
+          </div>
+        </div>
+        <div className="chek-bottom">
+          <h1>“ILMPLYUS O‘QUV” NTM</h1>
+          <h1>STIR | 303451853</h1>
+          <img src={QRCode} className="qr-code" />
+          <p>www.ilmplyus.uz</p>
+        </div>
+      </section>
+    </>
   );
 };
 
